5b6d7e2c5153cb085dcd15371856943e172fa2d4
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-10-17 11:32:44 -0500

    Modelo de la tabla de categorias

---
+++ db/models/category.model.js
@@ -0,0 +1,41 @@
+const { Model, Sequelize, DataTypes } = require('sequelize');
+
+const CATEGORY_TABLE = 'categories';
+
+const CategorySchema = {
+	id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		autoIncrement: true,
+		primaryKey: true,
+	},
+	name: {
+		type: DataTypes.STRING,
+		unique: true,
+		allowNull: false,
+	},
+	image: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	createdAt: {
+		type: DataTypes.STRING,
+		field: 'created_at',
+		allowNull: false,
+		defaultValue: Sequelize.NOW,
+	},
+};
+
+class Category extends Model {
+	static associate() {}
+	static config(sequelize) {
+		return {
+			sequelize,
+			modelName: 'Category',
+			tableName: CATEGORY_TABLE,
+			timestamps: false,
+		};
+	}
+}
+
+module.exports = { CATEGORY_TABLE, CategorySchema, Category };
--- db/models/index.js
+++ db/models/index.js
@@ -1,11 +1,13 @@
 const { User, UserSchema } = require('./user.model');
 const { Customer, CustomerSchema } = require('./customer.model');
 const { Product, ProductSchema } = require('./product.model');
+const { Category, CategorySchema } = require('./category.model');
 
 function setupModels(sequelize) {
 	User.init(UserSchema, User.config(sequelize));
 	User.init(CustomerSchema, Customer.config(sequelize));
 	User.init(ProductSchema, Product.config(sequelize));
+	User.init(CategorySchema, Category.config(sequelize));
 }
 
 module.exports = setupModels;
