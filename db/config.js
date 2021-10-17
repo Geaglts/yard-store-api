6da690d78f7ed9db2e6bbd09680d7707bfab094f
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-10-16 22:58:25 -0500

    Primera migracion para crear la tabla usuarios

--- db/config.js
+++ db/config.js
@@ -1,17 +1,16 @@
-const { Sequelize } = require('sequelize');
-
-const setupModels = require('../db/models');
 const { config } = require('../config/config');
 
 const USER = encodeURIComponent(config.pgUser);
 const PASSWORD = encodeURIComponent(config.pgPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.pgHost}:${config.pgPort}/${config.pgDatabase}`;
 
-const sequelize = new Sequelize(URI, {
-	dialect: 'postgres',
-	logging: true,
-});
-
-setupModels(sequelize);
-
-module.exports = sequelize;
+module.exports = {
+	development: {
+		url: URI,
+		dialect: 'postgres',
+	},
+	production: {
+		url: URI,
+		dialect: 'postgres',
+	},
+};
