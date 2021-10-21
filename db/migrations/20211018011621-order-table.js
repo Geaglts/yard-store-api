d8ad8a22877b4526484e257f5c8b10793740b171
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-10-20 22:23:02 -0500

    custom schema in order migration

--- db/migrations/20211018011621-order-table.js
+++ db/migrations/20211018011621-order-table.js
@@ -1,10 +1,36 @@
 'use strict';
+const { DataTypes, Sequelize } = require('sequelize');
 
-const { ORDER_TABLE, OrderSchema } = require('../models/order.model');
+const { ORDER_TABLE } = require('../models/order.model');
+const { CUSTOMER_TABLE } = require('../models/customer.model');
 
 module.exports = {
 	up: async (queryInterface) => {
-		await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+		await queryInterface.createTable(ORDER_TABLE, {
+			id: {
+				primaryKey: true,
+				type: DataTypes.INTEGER,
+				autoIncrement: true,
+				allowNull: false,
+			},
+			createdAt: {
+				defaultValue: Sequelize.NOW,
+				allowNull: false,
+				type: DataTypes.DATE,
+				field: 'created_at',
+			},
+			customerId: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				field: 'customer_id',
+				references: {
+					model: CUSTOMER_TABLE,
+					key: 'id',
+				},
+				onUpdate: 'CASCADE',
+				onDelete: 'SET NULL',
+			},
+		});
 	},
 	down: async (queryInterface) => {
 		await queryInterface.dropTable(ORDER_TABLE);
